d19cd19e3814cb2d0fd6c50cf49ca1085fda9f48
Centralice <108938330+Centralice@users.noreply.github.com>
2024-04-21 18:01:31 +0200

    hooray!

--- js/task-1.js
+++ js/task-1.js
@@ -1,20 +1,36 @@
-// Станція з продажу ремонтних дроїдів готова до запуску, залишилося написати програмне забезпечення для відділу продажів.
+// Перед звільненням розробник зламав вихідний код управління акаунтами користувачів нашого сервісу доставки їжі.
+// Виконай рефакторинг методів обєкта customer, розставивши відсутні this під час звернення до властивостей обєкта.
 
-// Оголоси функцію makeTransaction, яка очікує два параметри, значення яких будуть задаватися під час її виклику:
-// • quantity— перший параметр, число, що містить кількість замовлених дроїдів
-// • pricePerDroid — другий параметр, число, що містить вартість одного дроїда
 
-// Доповни код функції так, щоб вона повертала рядок з повідомленням про покупку ремонтних дроїдів:
-// "You ordered <quantity> droids worth <totalPrice> credits!", де:
-// • <quantity> — це кількість замовлених дроїдів
-// • <totalPrice> — це загальна вартість замовлення, тобто вартість усіх замовлених дроїдів
 
-function makeTransaction(quantity, pricePerDroid) {
-  return `You ordered ${quantity} droids worth ${
-    quantity * pricePerDroid
-  } credits!`;
-}
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  // Change code below this line
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Change code above this line
+};
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
--- js/task-2.js
+++ js/task-2.js
@@ -1,17 +1,53 @@
-// Оголоси функцію getShippingMessage, яка очікує три параметри, значення яких будуть задаватися під час її виклику:
-// • country — перший параметр, рядок, що містить країну доставки
-// • price — другий параметр, число, що містить загальну вартість товару
-// • deliveryFee — третій параметр, число, що містить вартість доставки товару
+// Створи клас Storage, який створюватиме обєкти для управління складом товарів.
+// Клас очікує лише один аргумент — початковий масив товарів,
+// який записується до створеного обєкта в приватну властивість items.
 
-// Доповни код функції так, щоб вона повертала рядок з повідомленням про доставку товару в країну користувача:
-// "Shipping to <country> will cost <totalPrice> credits", де:
-// • <country> — це країни доставки
-// • <totalPrice> — це загальна вартість замовлення, що включає вартість товару і його доставки
+// Оголоси наступні методи класу:
 
-function getShippingMessage(country, price, deliveryFee) {
-  return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
+// getItems() — повертає масив поточних товарів у приватній властивості items.
+// addItem(newItem) — приймає новий товар newItem і додає його до масиву товарів у приватну властивість items обєкта.
+// removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove
+// і видаляє його з масиву товарів у приватній властивості items обєкта.
+
+
+// function removeName(nameToRemove) {
+//     const names = ['Kate', 'Jack', 'Charlie'];
+//     const filteredNames = names.filter(name => name !== nameToRemove);
+//     return filteredNames;
+
+// }
+//   console.log(removeName("Jack"));
+
+
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const newItemList = this.#items.filter(item => item !== itemToRemove);
+    this.#items = newItemList;
+  }
 }
 
-console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage('Germany', 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage('Sweden', 100, 20)); // "Shipping to Sweden will cost 120 credits"
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+
